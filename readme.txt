8ea3ed7cf909bf27faccf46da2c4109f50da01c3
wasd11203 <383684855@qq.com>
2017-03-27 17:17:17 +0800

    this project is spring boot demo.it's incloude spring boot only. 这是一个spring boot 的示例。只包含 spring boot

---
+++ readme.txt
@@ -0,0 +1,2 @@
+ hello world 项目:
+主要展示了 spring boot 项目的基本项目结构.
---
+++ src/main/java/com/telincn/SpringWEBApplication.java
@@ -0,0 +1,13 @@
+package com.telincn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWEBApplication {
+	
+	public static void main(String[] args) {
+		 SpringApplication.run(SpringWEBApplication.class);  
+	}
+	
+}
